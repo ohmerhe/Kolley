93e4fef342cd96992ce4c3ed2e3c3d579e827a07
heliangwei <heliangwei@hujiang.com>
2016-04-12 21:51:10 +0800

    modify: request test

--- app/src/main/java/com/ohmerhe/kolley/demo/MainActivity.kt
+++ app/src/main/java/com/ohmerhe/kolley/demo/MainActivity.kt
@@ -2,11 +2,31 @@ package com.ohmerhe.kolley.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.ohmerhe.kolley.request.request
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        request<Weather>(this) {
+
+            url("http://api.openweathermap.org/data/2.5/weather")
+
+            params {
+                "q" - "shanghai"
+            }
+
+            success { weather ->
+                Log.d("MainActivity", weather.toString())
+            }
+
+            fail { error ->
+                Log.d("MainActivity", error.toString())
+            }
+
+        }
     }
 }
