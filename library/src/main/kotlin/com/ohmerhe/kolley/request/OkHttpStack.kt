e7e9a2cfa610db8e30e4cea94eebd94f76c661c6
heliangwei <heliangwei@hujiang.com>
2016-04-13 22:25:16 +0800

    modify: OkHttpStack not null param

--- library/src/main/kotlin/com/ohmerhe/kolley/request/OkHttpStack.kt
+++ library/src/main/kotlin/com/ohmerhe/kolley/request/OkHttpStack.kt
@@ -13,13 +13,10 @@ import java.net.URL
  * An [HttpStack][com.android.volley.toolbox.HttpStack] implementation which
  * uses OkHttp as its transport.
  */
-class OkHttpStack @JvmOverloads constructor(client: OkHttpClient? = OkHttpClient()) : HurlStack() {
+class OkHttpStack @JvmOverloads constructor(client: OkHttpClient = OkHttpClient()) : HurlStack() {
     private val mFactory: OkUrlFactory
 
     init {
-        if (client == null) {
-            throw NullPointerException("Client must not be null.")
-        }
         mFactory = OkUrlFactory(client)
     }
 
